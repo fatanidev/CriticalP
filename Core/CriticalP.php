a3748e401df384f362203df02d87fc688bfa9bf3
fatanidev <sabbamf@gmail.com>
2020-09-04 07:59:23 +0100

    Created Critical P Class and Interface

---
+++ Core/CriticalP.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "CriticalPManagementInterface.php";
+
+class CriticalP implements CriticalPtManagementInterface{
+  
+  
+	// Interface Functions
+	public function logon(){
+	}
+	public function logout(){
+	}
+	public function ParseWearableDeviceData(){
+	}		
+}
+
+?>
---
+++ Core/CriticalPManagementInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+interface CriticalPManagementInterface {
+	public function logon();
+	public function logout();
+	public function ParseWearableDeviceData();	
+?>
